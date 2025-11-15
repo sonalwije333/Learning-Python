b83ffe72fd7dfd5b2a2726c6ba8c2995a08ee4c4
Sonal Wijenayaka <sonalwijenayaka@gmail.com>
2025-11-16 02:20:55 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+cart = ['lamp', 'candles', 'chair', 'carpet']
+print(cart[:2])
+print(cart[:3])
+print(cart[2:])
+print(cart[:])\
+
+options = ['A', 'B', 'C', 'D']
+print(options[1:3])
+
+c = ['$', '£', '€', '¥']
+print(cart[-1])
+
+word = "movie"
+print('o' in word)
+
+words = ['code', 'program', 'learn']
+for i in words:
+  print(i)
